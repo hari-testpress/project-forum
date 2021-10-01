4397664d2690b60da0d3a6b4c14990822a69352d
hari-testpress <hari@testpress.in>
2021-10-01 21:38:39 +0530

    Support user registration in the forum app

--- myproject/account/urls.py
+++ myproject/account/urls.py
@@ -8,4 +8,9 @@ app_name = "account"
 urlpatterns = [
     path("signup/", views.signup, name="signup"),
     path("logout/", auth_view.LogoutView.as_view(), name="logout"),
+    path(
+        "login/",
+        auth_view.LoginView.as_view(template_name="login.html"),
+        name="login",
+    ),
 ]

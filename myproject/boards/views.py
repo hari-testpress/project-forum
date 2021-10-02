bd13fcacd5372e05ceae475756823b7a9020eafb
hari-testpress <hari@testpress.in>
2021-10-02 20:17:11 +0530

    Create list view for the posts in the topic

--- myproject/boards/views.py
+++ myproject/boards/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 
 from .forms import NewTopicForm
-from .models import Board, Post
+from .models import Board, Post, Topic
 
 
 def index(request):
@@ -36,3 +36,8 @@ def add_new_topic(request, pk):
     else:
         form = NewTopicForm()
     return render(request, "new_topic.html", {"board": board, "form": form})
+
+
+def topic_posts(request, pk, topic_pk):
+    topic = get_object_or_404(Topic, board__pk=pk, pk=topic_pk)
+    return render(request, "topic_posts.html", {"topic": topic})

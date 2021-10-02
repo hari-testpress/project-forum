ad35b88400038005150fa18612fb3ec7f6a4f532
hari-testpress <hari@testpress.in>
2021-10-02 13:51:08 +0530

    Use custom login template

--- myproject/account/urls.py
+++ myproject/account/urls.py
@@ -1,16 +1,10 @@
-from django.urls import path
-from django.contrib.auth import views as auth_view
+from django.urls import path, include
 
 from . import views
 
 app_name = "account"
 
 urlpatterns = [
+    path("", include("django.contrib.auth.urls")),
     path("signup/", views.signup, name="signup"),
-    path("logout/", auth_view.LogoutView.as_view(), name="logout"),
-    path(
-        "login/",
-        auth_view.LoginView.as_view(template_name="login.html"),
-        name="login",
-    ),
 ]

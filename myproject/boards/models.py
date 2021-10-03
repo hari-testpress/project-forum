198dc84fa360f1789eadc5c80b650dd3c1807013
hari-testpress <hari@testpress.in>
2021-10-03 20:59:14 +0530

    Display post's paginator on the topic list view

--- myproject/boards/models.py
+++ myproject/boards/models.py
@@ -4,6 +4,8 @@ from django.contrib.auth.models import User
 from django.utils.html import mark_safe
 from markdown import markdown
 
+import math
+
 
 class Board(models.Model):
     name = models.CharField(max_length=25, unique=True)
@@ -25,6 +27,25 @@ class Topic(models.Model):
     board = models.ForeignKey(Board, related_name="topics", on_delete=models.CASCADE)
     starter = models.ForeignKey(User, related_name="topics", on_delete=models.CASCADE)
     views_count = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return self.subject
+
+    def get_page_count(self):
+        count = self.posts.count()
+        pages = count / 2
+        return math.ceil(pages)
+
+    def has_many_pages(self, count=None):
+        if count is None:
+            count = self.get_page_count()
+        return count > 6
+
+    def get_page_range(self):
+        count = self.get_page_count()
+        if self.has_many_pages(count):
+            return range(1, 5)
+        return range(1, count + 1)
 
 
 class Post(models.Model):

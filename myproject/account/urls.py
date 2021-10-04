5f4109378192635636e344a987fd56915469426a
hari-testpress <89839073+hari-testpress@users.noreply.github.com>
2021-10-04 12:34:26 +0530

    Support user logout (#11)

--- myproject/account/urls.py
+++ myproject/account/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
+from django.contrib.auth import views as auth_view
+
 from . import views
 
 app_name = "account"
 
 urlpatterns = [
     path("signup/", views.signup, name="signup"),
+    path("logout/", auth_view.LogoutView.as_view(), name="logout"),
 ]

9265a5fb357fc7d3d6b3a75ab263ffe32a0d4112
hari-testpress <89839073+hari-testpress@users.noreply.github.com>
2021-10-04 12:44:31 +0530

    Support user login in the forum app (#14)

--- myproject/account/urls.py
+++ myproject/account/urls.py
@@ -1,11 +1,10 @@
-from django.urls import path
-from django.contrib.auth import views as auth_view
+from django.urls import path, include
 
 from . import views
 
 app_name = "account"
 
 urlpatterns = [
+    path("", include("django.contrib.auth.urls")),
     path("signup/", views.signup, name="signup"),
-    path("logout/", auth_view.LogoutView.as_view(), name="logout"),
 ]

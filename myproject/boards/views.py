237f0c4e118978e6333e32420a18aa2fb85d6ada
hari-testpress <89839073+hari-testpress@users.noreply.github.com>
2021-10-04 19:53:49 +0530

    Change topic and post list views to class based views (#33)

--- myproject/boards/views.py
+++ myproject/boards/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from django.db.models import Count
-from django.views.generic import UpdateView
+from django.views.generic import UpdateView, ListView
 from django.utils import timezone
 from django.utils.decorators import method_decorator
 
@@ -15,12 +15,21 @@ def index(request):
     return render(request, "index.html", {"boards": boards})
 
 
-def board_topics(request, pk):
-    board = get_object_or_404(Board, pk=pk)
-    topics = board.topics.order_by("-created_at").annotate(
-        replies=Count("posts") - 1
-    )
-    return render(request, "topics.html", {"board": board, "topics": topics})
+class TopicListView(ListView):
+    model = Topic
+    context_object_name = "topics"
+    template_name = "topics.html"
+
+    def get_context_data(self, **kwargs):
+        kwargs["board"] = self.board
+        return super().get_context_data(**kwargs)
+
+    def get_queryset(self):
+        self.board = get_object_or_404(Board, pk=self.kwargs.get("pk"))
+        queryset = self.board.topics.order_by("-created_at").annotate(
+            replies=Count("posts") - 1
+        )
+        return queryset
 
 
 @login_required
@@ -45,11 +54,25 @@ def add_new_topic(request, pk):
     return render(request, "new_topic.html", {"board": board, "form": form})
 
 
-def topic_posts(request, pk, topic_pk):
-    topic = get_object_or_404(Topic, board__pk=pk, pk=topic_pk)
-    topic.views_count += 1
-    topic.save()
-    return render(request, "topic_posts.html", {"topic": topic})
+class PostListView(ListView):
+    model = Post
+    context_object_name = "posts"
+    template_name = "topic_posts.html"
+
+    def get_context_data(self, **kwargs):
+        self.topic.views += 1
+        self.topic.save()
+        kwargs["topic"] = self.topic
+        return super().get_context_data(**kwargs)
+
+    def get_queryset(self):
+        self.topic = get_object_or_404(
+            Topic,
+            board__pk=self.kwargs.get("pk"),
+            pk=self.kwargs.get("topic_pk"),
+        )
+        queryset = self.topic.posts.order_by("created_at")
+        return queryset
 
 
 @login_required

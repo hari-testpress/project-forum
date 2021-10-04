0016ea5e2c479624f4b49a45b612d5dd67b61c01
hari-testpress <89839073+hari-testpress@users.noreply.github.com>
2021-10-04 12:18:15 +0530

    Support adding new topics to the board (#6)

--- myproject/boards/views.py
+++ myproject/boards/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Board
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.models import User
+
+from .forms import NewTopicForm
+from .models import Board, Post
 
 
 def index(request):
@@ -10,3 +13,24 @@ def index(request):
 def board_topics(request, pk):
     board = get_object_or_404(Board, pk=pk)
     return render(request, "topics.html", {"board": board})
+
+
+def add_new_topic(request, pk):
+    board = get_object_or_404(Board, pk=pk)
+    user = User.objects.first()
+    if request.method == "POST":
+        form = NewTopicForm(request.POST)
+        if form.is_valid():
+            topic = form.save(commit=False)
+            topic.board = board
+            topic.starter = user
+            topic.save()
+            Post.objects.create(
+                message=form.cleaned_data.get("message"),
+                topic=topic,
+                created_by=user,
+            )
+            return redirect("board:board_topics", pk=board.pk)
+    else:
+        form = NewTopicForm()
+    return render(request, "new_topic.html", {"board": board, "form": form})

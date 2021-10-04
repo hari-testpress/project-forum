818c572430ad5fdb1070c6c1d1ac8e0cd78764ad
hari-testpress <hari@testpress.in>
2021-10-04 19:45:38 +0530

    Refactor the view

--- myproject/boards/tests/test_view_topic_posts.py
+++ myproject/boards/tests/test_view_topic_posts.py
@@ -3,20 +3,16 @@ from django.test import TestCase
 from django.urls import resolve, reverse
 
 from ..models import Board, Post, Topic
-from ..views import topic_posts
+from ..views import PostListView
 
 
 class TopicPostsTests(TestCase):
     def setUp(self):
-        board = Board.objects.create(
-            name="Django", description="Django board."
-        )
+        board = Board.objects.create(name="Django", description="Django board.")
         user = User.objects.create_user(
             username="john", email="john@doe.com", password="123"
         )
-        topic = Topic.objects.create(
-            subject="Hello, world", board=board, starter=user
-        )
+        topic = Topic.objects.create(subject="Hello, world", board=board, starter=user)
         Post.objects.create(
             message="Lorem ipsum dolor sit amet", topic=topic, created_by=user
         )
@@ -30,4 +26,4 @@ class TopicPostsTests(TestCase):
 
     def test_url_resolves_topic_posts_function(self):
         view = resolve("/boards/1/topics/1/")
-        self.assertEquals(view.func, topic_posts)
+        self.assertEquals(view.func.view_class, PostListView)

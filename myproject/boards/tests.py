9890abbfa5f4f203fc677b35bf758b5685a07025
hari-testpress <89839073+hari-testpress@users.noreply.github.com>
2021-10-04 12:06:50 +0530

    Display available boards in the index view (#3)
    * Display available boards in the index view
    
    * Create unit tests for the index view

--- myproject/boards/tests.py
+++ myproject/boards/tests.py
@@ -1,3 +1,16 @@
+from django.urls import reverse
 from django.test import TestCase
+from django.urls import resolve
+from .models import Board
+from .views import index
 
-# Create your tests here.
+
+class IndexViewTests(TestCase):
+    def test_status_code_is_200(self):
+        url = reverse("index")
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 200)
+
+    def test_root_url_resolves_index_view(self):
+        view = resolve("/")
+        self.assertEquals(view.func, index)

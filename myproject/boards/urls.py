6624326a89ecccc52f6d2df5772e1fd8df1b75d4
hari-testpress <89839073+hari-testpress@users.noreply.github.com>
2021-10-04 12:15:09 +0530

    Support displaying topics from the particular board (#5)

---
+++ myproject/boards/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+app_name = "board"
+
+urlpatterns = [
+    path("<int:pk>/", views.board_topics, name="topics"),
+]
--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from boards import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", views.index, name="index"),
+    path("boards/", include("boards.urls", namespace="board")),
 ]
